e0e47a41d9a6d853d26442018031062b4269c0cc
Dominik <d.bladek@gmail.com>
2019-06-24 22:00:26 +0200

    LoL cog linted

--- ext/leagueoflegends.py
+++ ext/leagueoflegends.py
@@ -1,23 +1,25 @@
 # Import the commands extension
 from discord.ext import commands
 import os
-import sys
 
-BASE_URL="https://na1.api.riotgames.com"
-SUMMONER_API="/lol/summoner/v4/summoners/by-name/{}?api_key={}"
+BASE_URL = "https://na1.api.riotgames.com"
+SUMMONER_API = "/lol/summoner/v4/summoners/by-name/{}?api_key={}"
+
 
 class LeagueCog(commands.Cog):
     def __init__(self, bot):
         # Save our bot for later use
         self.bot = bot
-        self.league_key=os.environ["LEAGUE_TOKEN"]
+        self.league_key = os.environ["LEAGUE_TOKEN"]
 
     @commands.command(name='lolprofile', aliases=["lp"])
     async def lolprofile(self, ctx, args):
         async with self.bot.session.get(BASE_URL + SUMMONER_API.format(args, self.league_key)) as resp:
-            if(resp.status == 404): await ctx.send("Summoner not found")
+            if(resp.status == 404):
+                await ctx.send("Summoner not found")
             data = await resp.json()
         await ctx.send(data.get("summonerLevel"))
+
 
 def setup(bot):
     if "LEAGUE_TOKEN" in os.environ:
