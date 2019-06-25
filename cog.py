3c93d136e971059123361d906b85bc8dfeee445e
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-25 13:12:27 -0400

    Added custom Cog class

---
+++ cog.py
@@ -0,0 +1,19 @@
+# Import the commands extension
+from discord.ext import commands
+
+
+class Cog(commands.Cog):
+    """
+    A custom Cog Class to comply with the GDPR.
+    """
+    async def dump(self) -> dict:
+        """
+        Dumps all of the Cog data into a dictionary.
+        """
+        return {}
+
+    async def forget(self) -> dict:
+        """
+        Removes all of the data from the cog.
+        """
+        return {}
