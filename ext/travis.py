1bcd6d68ece954dbfc071a80a1b7883a47d33631
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-24 23:09:46 -0400

    Added command to get the user repositories

--- ext/travis.py
+++ ext/travis.py
@@ -8,6 +8,7 @@ from exceptions import NoTokenSet
 BASE = "https://api.travis-ci.com"
 # A list of available endpoints
 EP_USER = "/user"
+EP_REPOS = "/repos"
 # Travis CI brand colors (https://travis-ci.com/logo)
 OXIDE_BLUE = 0x3EAAAF
 TURF_GREEN = 0x39AA56
@@ -97,6 +98,49 @@ class Travis(commands.Cog):
         if isinstance(ctx.channel, discord.TextChannel):
             # Delete the original message
             await ctx.message.delete()
+
+    @travis.command()
+    async def repos(self, ctx):
+        """
+        Lists all of the repositories that the User has access to.
+        """
+        # Send a typing
+        await ctx.trigger_typing()
+
+        # Get the current user token
+        token = (await self.get_token(ctx.author.id))["token"]
+        # Create a place to store the repository data
+        desc = ""
+
+        # Create a copy of the default haders
+        headers = copy.deepcopy(DEFAULT_HEADERS)
+        # Set the token specified by the user
+        headers["Authorization"] = f"token {token}"
+
+        # Request the list of user repos
+        async with self.bot.session.get(BASE + EP_REPOS, headers=headers) as resp:
+            # If we didn't got a code 200, notify the user and return
+            if resp.status != 200:
+                await ctx.send(f"Unable to get your list of repos: Code {resp.status}")
+                return
+
+            # Parse the response as JSON
+            json = await resp.json()
+
+        # Iterate over the list of repos
+        for repo in [x for x in json["repositories"] if x["active"] and not x["private"]]:
+            # And add the repo information
+            desc += "{0} ({1})\n".format(repo["slug"], repo["default_branch"]["name"])
+
+        # Create an embed
+        embed = discord.Embed()
+        embed.color = OXIDE_BLUE
+        embed.title = "{0}'s repositories on Travis CI".format(ctx.author.name)
+        embed.description = desc
+        embed.set_thumbnail(url="https://travis-ci.com/images/logos/TravisCI-Mascot-1.png")
+
+        # Finally, send the embed
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
