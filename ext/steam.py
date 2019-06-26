a5d4691b2a862cbf5ae91a86dcb63478582f09a9
Dominik <d.bladek@gmail.com>
2019-06-27 00:50:33 +0200

    Fixed linting and error

--- ext/steam.py
+++ ext/steam.py
@@ -2,9 +2,6 @@
 import discord
 from discord.ext import commands
 import os
-import requests
-import json
-import pprint
 from cog import Cog
 
 
@@ -12,45 +9,40 @@ BASE_URL = "https://api.steampowered.com"
 PROFILE_API = "/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}"
 
 
-
-#cog stuff
+# cog stuff
 class SteamCog(Cog):
-    
+
     def __init__(self, bot):
-        #save dat stuff 4 ltr
+        # save dat stuff 4 ltr
         self.bot = bot
         self.steam_key = os.environ["STEAM_TOKEN"]
 
-#don't know if this works
+# don't know if this works
     async def getProfileData(self, ctx, profile):
-            async with self.bot.session.get(BASE_URL + PROFILE_API.format(self. steam_key, profile)) as resp:
-                return await resp.json()
+        async with self.bot.session.get(BASE_URL + PROFILE_API.format(self. steam_key, profile)) as resp:
+            return await resp.json()
 
-#make the command
+# make the command
     @commands.command(name='profile', aliases=["myp"])
     async def profile(self, ctx, *args):
-        #creates embeded profile
+        # creates embeded profile
         steamprofile = '{}'.format(' '.join(args[1:]))
         data = await self.getProfileData(self, steamprofile)
         if not data:
             await ctx.send("profile not found")
             return
-        #do all the embed stuff
+        # do all the embed stuff
         embed = discord.Embed(title=data.get("personaname"))
         embed.set_author(name=("Real name: " + str(data.get("realname"))))
-        embed.set_thumbnail(url=(data.get("avatar"))))
-
-
-
-
+        embed.set_thumbnail(url=(data.get("avatar")))
         await ctx.send(embed=embed)
 
 
-#setup the bot ith cog
+# setup the bot ith cog
 def setup(bot):
 
     if "STEAM_TOKEN" in os.environ:
-    #if steam token findable launch the cog
+        # if steam token findable launch the cog
         bot.add_cog(SteamCog(bot))
 
     else:
