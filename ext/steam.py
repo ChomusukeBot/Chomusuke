bb259e88bed865c2f686d79c1d0da166424db694
makethpanda <makethpanda@users.noreply.github.com>
2019-06-27 02:50:34 +0200

    First working commit
    profile command added

--- ext/steam.py
+++ ext/steam.py
@@ -9,6 +9,7 @@ BASE_URL = "https://api.steampowered.com"
 PROFILE_API = "/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}"
 
 
+
 # cog stuff
 class SteamCog(Cog):
 
@@ -19,24 +20,32 @@ class SteamCog(Cog):
 
 # don't know if this works
     async def getProfileData(self, ctx, profile):
-        async with self.bot.session.get(BASE_URL + PROFILE_API.format(self. steam_key, profile)) as resp:
+        async with self.bot.session.get(BASE_URL + PROFILE_API.format(self.steam_key, profile)) as resp:
             return await resp.json()
+
+
 
 # make the command
     @commands.command(name='profile', aliases=["myp"])
-    async def profile(self, ctx, *args):
+    async def profile(self, ctx, char):
+        """
+        Generates an embed displaying the specified user's STEAM profile, needs steam id.
+        """
         # creates embeded profile
-        steamprofile = '{}'.format(' '.join(args[1:]))
+        steamprofile = char
         data = await self.getProfileData(self, steamprofile)
+        #await ctx.send(data)
+        #await ctx.send(steamprofile)
         if not data:
             await ctx.send("profile not found")
             return
         # do all the embed stuff
-        embed = discord.Embed(title=data.get("personaname"))
-        embed.set_author(name=("Real name: " + str(data.get("realname"))))
-        embed.set_thumbnail(url=(data.get("avatar")))
+        embed = discord.Embed(title=("Real name: " + (data.get("response").get("players")[0].get("realname"))))
+        #embed.set_thumbnail(url=(data.get("avatar")))       
+        embed.set_author(name=("Name:" + str(data.get("response").get("players")[0].get("personaname"))))
+        #embed.add_field(name="Country & City", value=("{} {}".format(data.get("response").get("players")[0].get("loccountrycode")), (data.get("response").get("players")[0].get("loccountrycode"))), inline=True)
+        embed.set_thumbnail(url=(data.get("response").get("players")[0].get("avatarmedium")))
         await ctx.send(embed=embed)
-
 
 # setup the bot ith cog
 def setup(bot):
