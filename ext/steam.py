9e0e201fc4c24dc768a104194aa2985942b2b70d
makethpanda <makethpanda@users.noreply.github.com>
2019-06-28 18:53:26 +0200

    finished and polished games
    finished and polished current games

--- ext/steam.py
+++ ext/steam.py
@@ -15,32 +15,39 @@ RUST_API = "/ISteamUserStats/GetUserStatsForGame/v0002/?appid=252490&key={}&stea
 
 # cog stuff
 class SteamCog(Cog):
-
+    """
+    a cog for steam stats for various games
+    """
     def __init__(self, bot):
         # save dat stuff 4 ltr
         self.bot = bot
         self.steam_key = os.environ["STEAM_TOKEN"]
 
-# don't know if this works
+#GET DATA
+
+    #profile
     async def getProfileData(self, ctx, profile):
         async with self.bot.session.get(BASE_URL + PROFILE_API.format(self.steam_key, profile)) as resp:
             return await resp.json()
 
+    #CSGOProfile
     async def getCSData(self, ctx, profile):
         async with self.bot.session.get(BASE_URL + CSGO_API.format(self.steam_key, profile)) as resp:
             return await resp.json()
 
+    #TF2Data
     async def getTFIIData(self, ctx, profile):
         async with self.bot.session.get(BASE_URL + TFII_API.format(self.steam_key, profile)) as resp:
             return await resp.json()
 
+    #RustData
     async def getRustData(self, ctx, profile):
         async with self.bot.session.get(BASE_URL + RUST_API.format(self.steam_key, profile)) as resp:
             return await resp.json()
 
 
 
-# make the command
+    # PROFILE COMMAND
     @commands.command(name='profile', aliases=["myp"])
     async def profile(self, ctx, char):
         """
@@ -62,8 +69,35 @@ class SteamCog(Cog):
         embed.set_thumbnail(url=(data.get("response").get("players")[0].get("avatarmedium")))
         await ctx.send(embed=embed)
 
-    # make the command
-    @commands.command(name='csgo', aliases=["csg"])
+
+    #PROFILE SEARCH Command
+    @commands.command(name="inprofile", aliases=["fip"])
+    async def inprofile(self, ctx, char, search):
+        """
+        Generates an embed displaying the specified user's STEAM profile with a custom search parameter
+        ex "inprofile [steam id] 
+        """
+        # get data and needed variables
+        steamprofile = char
+        findData = search
+        data = await self.getProfileData(self, steamprofile)
+
+        if not data:
+            await ctx.send("profile not found")
+            return
+
+        # create and send embeded
+        embed = discord.Embed(title=("Real name: " + (data.get("response").get("players")[0].get("realname"))))      
+        embed.set_author(name=("Name:" + str(data.get("response").get("players")[0].get("personaname"))))
+        embed.add_field(name=findData + "(custom):", value=data.get("response").get("players")[0].get(findData), inline=True) 
+        embed.set_thumbnail(url=(data.get("response").get("players")[0].get("avatarmedium")))
+        
+        await ctx.send(embed=embed)
+
+
+
+    #CSGO Command
+    @commands.command(name="csgo", aliases=["csg"])
     async def csgo(self, ctx, char):
         """
         embed displaying the specified user's stats for CSGO
@@ -82,7 +116,7 @@ class SteamCog(Cog):
         #kills = str([x for x in data2["playerstats"]["stats"] if x["name"] == ["total_kills"][0]["value"]])
         #deaths = str([x for x in data2["playerstats"]["stats"] if x["name"] == ["total_deaths"][0]["value"]])
 
-        # do all the embed stuff
+        # embed and send embeded
         embed = discord.Embed(title=("CSGO Stats for " + str(data.get("response").get("players")[0].get("personaname"))))
         embed.set_thumbnail(url=(data.get("response").get("players")[0].get("avatarmedium")))
         embed.add_field(name="Total Kills: ", value=str([x for x in data2["playerstats"]["stats"] if x["name"] == "total_kills"][0]["value"]), inline=True)
@@ -91,26 +125,28 @@ class SteamCog(Cog):
         embed.add_field(name="Total damage done: ", value=str([x for x in data2["playerstats"]["stats"] if x["name"] == "total_damage_done"][0]["value"]), inline=True)
         embed.add_field(name="Last match kills: ", value=str([x for x in data2["playerstats"]["stats"] if x["name"] == "last_match_kills"][0]["value"]), inline=True)
         embed.add_field(name="Last match deaths: ", value=str([x for x in data2["playerstats"]["stats"] if x["name"] == "last_match_deaths"][0]["value"]), inline=True)
+        
         await ctx.send(embed=embed)
 
 
 
-    # make the command
+    # TEAM FORTERESS TWO command
     @commands.command(name='tfii', aliases=["tf2"])
     async def tfii(self, ctx, char):
         """
         embed displaying the specified user's stats for 
-        Team forteress II, needs steam id code
+        Team forteress II
+        ex: tfii [steam ID code]
         """
-        # creates embeded profile
+        # creates vars needed
         steamprofile = char
         data = await self.getProfileData(self, steamprofile)
         data3 = await self.getTFIIData(self, steamprofile)
-        #await ctx.send(data)
-        #await ctx.send(steamprofile)
+        
         if not data3:
             await ctx.send("stats not found")
             return
+
         # do all the embed stuff
         embed = discord.Embed(title=("TF II Stats for " + str(data.get("response").get("players")[0].get("personaname"))))
         embed.set_thumbnail(url=(data.get("response").get("players")[0].get("avatarmedium")))
@@ -131,24 +167,23 @@ class SteamCog(Cog):
     async def rust(self, ctx, char):
         """
         embed displaying the specified user's stats for 
-        Rust, need steam id code
+        Rust
+        ex: rust [steam id code]
         """
-        # creates embeded profile
+        # get necessary data into vars
         steamprofile = char
         data = await self.getProfileData(self, steamprofile)
         data4 = await self.getRustData(self, steamprofile)
-        #await ctx.send(data)
-        #await ctx.send(steamprofile)
+
         if not data4:
             await ctx.send("stats not found")
             return
+
         # do all the embed stuff
         embed = discord.Embed(title=("Rust Stats for " + str(data.get("response").get("players")[0].get("personaname"))))
         embed.set_thumbnail(url=(data.get("response").get("players")[0].get("avatarmedium")))
         embed.add_field(name="Deaths: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "deaths"][0]["value"]), inline=True)
         embed.add_field(name="Kills: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "kill_player"][0]["value"]), inline=True)
-        embed.add_field(name="Calories consumed: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "calories_consumed"][0]["value"]), inline=True)
-        embed.add_field(name="Water consumed: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "water_consumed"][0]["value"]), inline=True)
         embed.add_field(name="Mele strikes: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "melee_strikes"][0]["value"]), inline=True)
         embed.add_field(name="Wounds: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "wounded"][0]["value"]), inline=True)
         await ctx.send(embed=embed)
