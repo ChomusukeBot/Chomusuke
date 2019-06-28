acbedd3a2a0d03e841737253a9f4d9d46a7b8cad
makethpanda <makethpanda@users.noreply.github.com>
2019-06-28 02:38:08 +0200

    RUST STATS FINISHED
    finished stats for rust

--- ext/steam.py
+++ ext/steam.py
@@ -9,6 +9,7 @@ BASE_URL = "https://api.steampowered.com"
 PROFILE_API = "/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}"
 CSGO_API = "/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key={}&steamid={}"
 TFII_API = "/ISteamUserStats/GetUserStatsForGame/v0002/?appid=440&key={}&steamid={}"
+RUST_API = "/ISteamUserStats/GetUserStatsForGame/v0002/?appid=252490&key={}&steamid={}"
 
 
 
@@ -31,6 +32,10 @@ class SteamCog(Cog):
 
     async def getTFIIData(self, ctx, profile):
         async with self.bot.session.get(BASE_URL + TFII_API.format(self.steam_key, profile)) as resp:
+            return await resp.json()
+
+    async def getRustData(self, ctx, profile):
+        async with self.bot.session.get(BASE_URL + RUST_API.format(self.steam_key, profile)) as resp:
             return await resp.json()
 
 
@@ -95,7 +100,7 @@ class SteamCog(Cog):
     async def tfii(self, ctx, char):
         """
         embed displaying the specified user's stats for 
-        Team forteress II, needs steam id
+        Team forteress II, needs steam id code
         """
         # creates embeded profile
         steamprofile = char
@@ -116,6 +121,39 @@ class SteamCog(Cog):
         embed.add_field(name="Engineer kills: ", value=str([x for x in data3["playerstats"]["stats"] if x["name"] == "Engineer.accum.iNumberOfKills"][0]["value"]), inline=True)
         embed.add_field(name="Engineer sentry kills: ", value=str([x for x in data3["playerstats"]["stats"] if x["name"] == "Engineer.max.iSentryKills"][0]["value"]), inline=True)
         await ctx.send(embed=embed)
+
+
+
+
+
+    # make the command
+    @commands.command(name='rust', aliases=["rst"])
+    async def rust(self, ctx, char):
+        """
+        embed displaying the specified user's stats for 
+        Rust, need steam id code
+        """
+        # creates embeded profile
+        steamprofile = char
+        data = await self.getProfileData(self, steamprofile)
+        data4 = await self.getRustData(self, steamprofile)
+        #await ctx.send(data)
+        #await ctx.send(steamprofile)
+        if not data4:
+            await ctx.send("stats not found")
+            return
+        # do all the embed stuff
+        embed = discord.Embed(title=("Rust Stats for " + str(data.get("response").get("players")[0].get("personaname"))))
+        embed.set_thumbnail(url=(data.get("response").get("players")[0].get("avatarmedium")))
+        embed.add_field(name="Deaths: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "deaths"][0]["value"]), inline=True)
+        embed.add_field(name="Kills: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "kill_player"][0]["value"]), inline=True)
+        embed.add_field(name="Calories Consumed: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "calories_consumed"][0]["value"]), inline=True)
+        embed.add_field(name="Water consumed: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "water_consumed"][0]["value"]), inline=True)
+        embed.add_field(name="Mele strikes: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "melee_strikes"][0]["value"]), inline=True)
+        embed.add_field(name="Wounds: ", value=str([x for x in data4["playerstats"]["stats"] if x["name"] == "wounded"][0]["value"]), inline=True)
+        await ctx.send(embed=embed)
+
+
 
 
 # setup the bot ith cog
