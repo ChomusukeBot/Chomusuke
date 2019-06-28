7d94217b9036263d10be261c74f4822ed85fb813
Rohan <icemam449@gmail.com>
2019-06-28 23:34:29 +0530

    reverting last commit, re adding the dict for league regions insted of list because i need to the names too.Also removing the useless fetch function

--- ext/api_status.py
+++ ext/api_status.py
@@ -20,7 +20,20 @@ ALL_AUTH = {
 BASE_URL = "https://{}.api.riotgames.com"
 SHARD_STATUS_URL = "/lol/status/v3/shard-data?api_key={}"
 
-RIOT_REGIONS = ["br1", "eun1", "euw1", "jp1", "kr", "la1", "la2", "na1", "oc1", "tr1", "ru", "pbe1"]
+RIOT_REGIONS = {
+    "br": "br1",
+    "eune": "eun1",
+    "euw": "euw1",
+    "jp": "jp1",
+    "kr": "kr",
+    "lan": "la1",
+    "las": "la2",
+    "na": "na1",
+    "oce": "oc1",
+    "tr": "tr1",
+    "ru": "ru",
+    "pbe": "pbe1"
+}
 
 
 class APIStatus(Cog):
@@ -31,10 +44,6 @@ class APIStatus(Cog):
         # Save our bot for later use
         self.bot = bot
         self.http_session = aiohttp.ClientSession()
-
-    async def fetch(self, url):
-        async with self.http_session.get(url) as response:
-            return await response.read()
 
     @commands.command(name="apistatus", aliases=["api", "status"])
     async def get_api_status(self, ctx):
@@ -53,8 +62,8 @@ class APIStatus(Cog):
                     embed.add_field(name=f"{key} API", value=emoji1, inline=True)
                 else:
                     embed.add_field(name=f"{key} API", value=emoji2, inline=True)
-        for region in RIOT_REGIONS:
-            url = BASE_URL.format(region) + SHARD_STATUS_URL.format(ALL_AUTH["api_key"])
+        for key, value in RIOT_REGIONS.items():
+            url = BASE_URL.format(value) + SHARD_STATUS_URL.format(ALL_AUTH["api_key"])
             async with self.http_session.get(url=url) as response:
                 print(response.status)
                 if response.status == 200:
