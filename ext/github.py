f66395da3dc7bdc0a8d1539859099b34ec0f05c3
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-28 14:33:55 -0400

    GitHub: Reorganized and rewriten certain part of the cog

--- ext/github.py
+++ ext/github.py
@@ -79,126 +79,81 @@ class GitHub(Repo):
             await ctx.send(url)
 
     @github.command(aliases=["r"])
-    async def repo(self, ctx, repo_name: str, author: str = None):
+    async def repo(self, ctx):
         """
-            A command to get information about a repository.
-            example - c!github r <repository name> <owner of repository>
-
-            Note -
-            <owner of repository> is not a compulsary argument.
-
+        Shows the information for your repository.
         """
-        embed = discord.Embed(colour=discord.Colour.blue())
-        if author is None:
-            embed = await self.search_repos(repo_name, 1)
-        else:
-            url = f"https://api.github.com/repos/{author}/{repo_name}"
-            data = await self.fetch(self.bot.session, url)
-            if data["message"]:
-                return await ctx.send("Invalid Repository name or owner name!")
-            embed.title = f"Name : {data['name']}"
-            embed.description = f"**:star:{data['stargazers_count']}/:fork_and_knife:{data['forks']}\n\n"
-            embed.description += f"**Owner:** {data['owner']['login']}\n"
-            embed.description += f"**Description:** {data['description'][0:50]}\n"
-            if len(data["description"]) > 50:
-                embed.description += "..."
-            else:
-                pass
-            embed.url = data["html_url"]
+        # Use the stored repo
+        repo = (await self.picks.find_one({"_id": ctx.author.id}))["slug"]
 
+        # Make the request
+        async with self.bot.session.get(f"https://api.github.com/repos/{repo}", headers=HEADERS, params=PARAMETERS) as resp:
+            # And parse the JSON
+            json = await resp.json()
+
+        # Create an embed and fill the
+        embed = discord.Embed()
+        embed.title = f"Information of {repo}"
+        embed.url = json["html_url"]
+        embed.description = "{0} 👀 / {1} ⭐ / {2} 🍴".format(json["watchers_count"], json["stargazers_count"], json["forks_count"])
+        embed.description += "\n\n{0}\n\n".format(json["description"])
+        if json["license"]:
+            embed.description += "Released under the " + json["license"]["name"] + "\n\n"
+        embed.description += "Created on {0}\n".format(json["created_at"])
+        embed.description += "Updated on {0}\n".format(json["updated_at"])
+        embed.description += "Last Push on {0}\n".format(json["pushed_at"])
+        if json["fork"]:
+            embed.set_footer("This is a fork.")
+        embed.set_thumbnail(url=json["owner"]["avatar_url"])
+        # Finally, send the embed
         await ctx.send(embed=embed)
-
-    @github.command(aliases=["s"])
-    async def search(self, ctx, repo_name: str):
-        """
-        Searches for repositories with the specified name or slug.
-        Please note that the search is limited to 5 results.
-        """
-        embed = await self.search_repos(repo_name, 5)
-        await ctx.send(embed=embed)
-
-    async def search_repos(self, repo_name, no_of_repos: int):
-        embed = discord.Embed(colour=discord.Colour.blue())
-        url = "https://api.github.com/search/repositories"
-        params = {
-            "q": repo_name,
-            "sort": "stars"
-        }
-        data = await self.fetch(self.bot.session, url, params)
-        if data["total_count"] == 0:
-            embed.title = "Repository not found!"
-            return embed
-        embed.url = f"https://github.com/search?q={repo_name}"
-        i = 0
-        for x in data["items"]:
-            if no_of_repos == 5:
-                # For search command.
-                embed.title = f"Search results for {repo_name}"
-                field_name = f"Repo: {x['owner']['login']}/{x['name']}"
-            else:
-                # For get repository command.
-                embed.title = f"Name: {repo_name}"
-                field_name = f"Owner: {x['owner']['login']}"
-                embed.url = x["html_url"]
-            i += 1
-            if x["description"] is None:
-                message = "No description avilable."
-            else:
-                message = x["description"][0:50]
-
-            value_field = f"**:star:{x['stargazers_count']}/:fork_and_knife:{x['forks']}\nDescription :** {message}...\nLink : {x['html_url']}\n"
-            if no_of_repos != 1:
-                if i != no_of_repos:
-                    value_field += "."
-                else:
-                    pass
-            embed.add_field(
-                name=field_name,
-                value=value_field,
-                inline=False
-            )
-            if i == no_of_repos:
-                break
-        return embed
 
     @github.command(aliases=["i"])
-    async def issue(self, ctx, owner: str, repo: str, issue_id: int):
+    async def issue(self, ctx, _id: int):
         """
         Gets the complete information about an issue.
         """
-        url = f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_id}"
-        data = await self.fetch(self.bot.session, url)
-        if data["message"]:
-            return await ctx.send("Invalid Repository name or owner name or issue number!")
-        embed = discord.Embed(colour=discord.Colour.blue())
-        embed.title = data["title"]
-        embed.url = data["html_url"]
-        embed.description = data["body"]
+        # Use the stored repo
+        repo = (await self.picks.find_one({"_id": ctx.author.id}))["slug"]
 
-        if data["assignee"] is not None:
-            # If no one is assigned to the issue.
-            msg = data["assignee"]["login"]
-        else:
-            msg = "None"
-        embed.set_footer(text=f"Issue opened by {data['user']['login']} and is assigned to {msg}")
+        # Make the request
+        async with self.bot.session.get(f"https://api.github.com/repos/{repo}/issues/{_id}", headers=HEADERS, params=PARAMETERS) as resp:
+            # And parse the JSON
+            json = await resp.json()
 
+        # Create an embed and add the basic data
+        embed = discord.Embed()
+        embed.title = json["title"]
+        embed.url = json["html_url"]
+        embed.description = json["body"]
+        embed.set_footer(text=f"Issue opened by {json['user']['login']}")
+
+        # If the total data length of the embed is higher than 200
         if len(embed) > 2000:
-            # Checking if the embed exceeds 2000 chars to avoid BAD REQUEST.
-            embed.description = data["body"][0:1900] + "..."
+            # Trim the last 100 characters just in case
+            embed.description = json["body"][0:1900] + "..."
+        # Finally, send the embed
         await ctx.send(embed=embed)
 
-    @commands.group(aliases=["l"])
-    async def labels(self, ctx, owner: str, repo: str):
+    @github.group(aliases=["l"])
+    async def labels(self, ctx):
         """
         Gets all the labels in a repository.
         """
-        url = f"https://api.github.com/repos/{owner}/{repo}/labels"
-        data = await self.fetch(self.bot.session, url)
-        embed = discord.Embed(colour=discord.Colour.blue())
-        embed.title = "Labels"
+        # Use the stored repo
+        repo = (await self.picks.find_one({"_id": ctx.author.id}))["slug"]
+        # Make the request
+        async with self.bot.session.get(f"https://api.github.com/repos/{repo}/labels", headers=HEADERS, params=PARAMETERS) as resp:
+            # And parse the JSON
+            json = await resp.json()
+
+        # Create an embed and format it
+        embed = discord.Embed()
+        embed.title = "Labels of {0}".format(repo)
         embed.description = ""
-        for i, label in enumerate(data, start=1):
-            embed.description += f"{i}. **{label['name']}**\n"
+        for label in json:
+            embed.description += "*{0}* (#{1})\n".format(label["name"], label["color"].upper())
+        # Finally, send the embed
         await ctx.send(embed=embed)
 
 
