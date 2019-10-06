b03554628d27b2dcbc8117ada0636e954e046061
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-06 05:46:59 -0300

    Added parsing of configuration from environment variables

--- chomusuke/__main__.py
+++ chomusuke/__main__.py
@@ -1,6 +1,21 @@
 import argparse
+import os
+import sys
 
 from .bot import Chomusuke
+
+
+def config_from_env():
+    """
+    Generates a dict with configuration values from the environment variables.
+    """
+    # Create a dictionary with the info that we need
+    output = {
+        "token": os.environ["DISCORD_TOKEN"],
+        "prefix": os.environ.get("DISCORD_PREFIX", "&")
+    }
+    # And return it
+    return output
 
 
 def parse_args():
@@ -22,6 +37,19 @@ def main():
     """
     # Get the parsed command line arguments
     args = parse_args()
+
+    # If the user wants the config from environment variables
+    if args.env:
+        # If there is no token on the environment variables, exit
+        if "DISCORD_TOKEN" not in os.environ:
+            pass
+
+        # Otherwise, load the configuration
+        config = config_from_env()
+    # If there is no configuration type specified
+    else:
+        print("Error: You need to specify a configuration system")
+        sys.exit(2)
 
 
 if __name__ == "__main__":
