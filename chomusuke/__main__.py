e250f7432941290bf6c7ca418c7d83304d134fca
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-06 04:42:15 -0300

    Added function for parsing the cmd arguments

--- chomusuke/__main__.py
+++ chomusuke/__main__.py
@@ -1,3 +1,5 @@
+import argparse
+
 from .bot import Chomusuke
 
 
@@ -5,12 +7,21 @@ def parse_args():
     """
     Parses the command line arguments.
     """
+    # Create an instance of the Argument Parser
+    parser = argparse.ArgumentParser()
+    # Then, add the required arguments for this
+    parser.add_argument("--env", dest="env", action="store_true",
+                        help="if the bot should be configured with environment variables")
+    # Finally, return the arguments
+    return parser.parse_args()
 
 
 def main():
     """
     Executes the bot from the command line.
     """
+    # Get the parsed command line arguments
+    args = parse_args()
 
 
 if __name__ == "__main__":
