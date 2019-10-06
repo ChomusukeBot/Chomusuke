a219cbb6f77f74a844e57208dee64773c24370a8
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-06 03:51:17 -0300

    Added basic functions for launching the bot

---
+++ chomusuke/__main__.py
@@ -0,0 +1,17 @@
+from .bot import Chomusuke
+
+
+def parse_args():
+    """
+    Parses the command line arguments.
+    """
+
+
+def main():
+    """
+    Executes the bot from the command line.
+    """
+
+
+if __name__ == "__main__":
+    main()
